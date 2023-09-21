a3b4e21be07f085c6e7f3c2f6bc62144762e685c
baby-joda <olle.svanholm@gmail.com>
2023-09-21 23:49:34 +0200

    Fixed grid system and where to place the targets

--- UltimateTicTacToe/Form.cs
+++ UltimateTicTacToe/Form.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,73 @@ namespace UltimateTicTacToe
 {
     /*
         pbx size = (44; 44)
-        mellanrum mellan rutor och outline = 7px
-        start för xy på de olika 3x3 rutorna...
+        mellanrum mellan rutor = 51px
+        tillägg mellan 3x3 = 3px
     */
 
     public partial class Form : System.Windows.Forms.Form
     {
+        int targetSize = 51;
+        int targetPosX;
+        int targetPosY;
+
         public Form()
         {
             InitializeComponent();
         }
+
+        private void pbxPlayingBoard_MouseClick(object sender, MouseEventArgs e)
+        {
+            CalculateSquare(e.X, e.Y);
+        }
+
+        void CalculateSquare(int mouseX, int mouseY)
+        {
+            int posInGridX = mouseX / targetSize;
+            int posInGridY = mouseY / targetSize;
+
+            targetPosX = FindStartPosX(posInGridX);
+            targetPosY = FindStartPosY(posInGridY);
+        }
+
+        int FindStartPosX(int posX)
+        {
+            int targetPosX;
+
+            if (posX >= 0 && posX <= 2)
+            {
+                targetPosX = (posX * 51) + 6;
+            }
+            else if (posX >= 3 && posX <= 5)
+            {
+                targetPosX = (posX * 51) + 9;
+            }
+            else
+            {
+                targetPosX = (posX * 51) + 12;
+            }
+
+            return targetPosX;
+        }
+
+        int FindStartPosY(int posY)
+        {
+            int targetPosY;
+
+            if (posY >= 0 && posY <= 2)
+            {
+                targetPosY = (posY * 51) + 6;
+            }
+            else if (posY >= 3 && posY <= 5)
+            {
+                targetPosY = (posY * 51) + 9;
+            }
+            else
+            {
+                targetPosY = (posY * 51) + 12;
+            }
+
+            return targetPosY;
+        }  
     }
 }
