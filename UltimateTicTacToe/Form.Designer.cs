d599d4d8b7358301075bd2be0ade14d5ff75bad6
baby-joda <olle.svanholm@gmail.com>
2023-09-25 23:24:10 +0200

    Smart way of solving this game

--- UltimateTicTacToe/Form.Designer.cs
+++ UltimateTicTacToe/Form.Designer.cs
@@ -28,39 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
-            this.pbxPlayingBoard = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pbxPlayingBoard)).BeginInit();
+            this.flpOne = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpTwo = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpThree = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpFive = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpFour = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpSix = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpEight = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpSeven = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpNine = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
-            // pbxPlayingBoard
+            // flpOne
             // 
-            this.pbxPlayingBoard.Image = global::UltimateTicTacToe.Properties.Resources.PlayingBoard;
-            this.pbxPlayingBoard.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbxPlayingBoard.InitialImage")));
-            this.pbxPlayingBoard.Location = new System.Drawing.Point(-1, -1);
-            this.pbxPlayingBoard.Name = "pbxPlayingBoard";
-            this.pbxPlayingBoard.Size = new System.Drawing.Size(472, 472);
-            this.pbxPlayingBoard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pbxPlayingBoard.TabIndex = 0;
-            this.pbxPlayingBoard.TabStop = false;
-            this.pbxPlayingBoard.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbxPlayingBoard_MouseClick);
+            this.flpOne.BackColor = System.Drawing.Color.DimGray;
+            this.flpOne.Location = new System.Drawing.Point(3, 3);
+            this.flpOne.Margin = new System.Windows.Forms.Padding(0);
+            this.flpOne.Name = "flpOne";
+            this.flpOne.Size = new System.Drawing.Size(150, 150);
+            this.flpOne.TabIndex = 81;
+            // 
+            // flpTwo
+            // 
+            this.flpTwo.BackColor = System.Drawing.Color.DimGray;
+            this.flpTwo.Location = new System.Drawing.Point(155, 3);
+            this.flpTwo.Margin = new System.Windows.Forms.Padding(0);
+            this.flpTwo.Name = "flpTwo";
+            this.flpTwo.Size = new System.Drawing.Size(150, 150);
+            this.flpTwo.TabIndex = 82;
+            // 
+            // flpThree
+            // 
+            this.flpThree.BackColor = System.Drawing.Color.DimGray;
+            this.flpThree.Location = new System.Drawing.Point(307, 3);
+            this.flpThree.Margin = new System.Windows.Forms.Padding(0);
+            this.flpThree.Name = "flpThree";
+            this.flpThree.Size = new System.Drawing.Size(150, 150);
+            this.flpThree.TabIndex = 83;
+            // 
+            // flpFive
+            // 
+            this.flpFive.BackColor = System.Drawing.Color.DimGray;
+            this.flpFive.Location = new System.Drawing.Point(155, 155);
+            this.flpFive.Margin = new System.Windows.Forms.Padding(0);
+            this.flpFive.Name = "flpFive";
+            this.flpFive.Size = new System.Drawing.Size(150, 150);
+            this.flpFive.TabIndex = 85;
+            // 
+            // flpFour
+            // 
+            this.flpFour.BackColor = System.Drawing.Color.DimGray;
+            this.flpFour.Location = new System.Drawing.Point(3, 155);
+            this.flpFour.Margin = new System.Windows.Forms.Padding(0);
+            this.flpFour.Name = "flpFour";
+            this.flpFour.Size = new System.Drawing.Size(150, 150);
+            this.flpFour.TabIndex = 84;
+            // 
+            // flpSix
+            // 
+            this.flpSix.BackColor = System.Drawing.Color.DimGray;
+            this.flpSix.Location = new System.Drawing.Point(307, 155);
+            this.flpSix.Margin = new System.Windows.Forms.Padding(0);
+            this.flpSix.Name = "flpSix";
+            this.flpSix.Size = new System.Drawing.Size(150, 150);
+            this.flpSix.TabIndex = 86;
+            // 
+            // flpEight
+            // 
+            this.flpEight.BackColor = System.Drawing.Color.DimGray;
+            this.flpEight.Location = new System.Drawing.Point(155, 307);
+            this.flpEight.Margin = new System.Windows.Forms.Padding(0);
+            this.flpEight.Name = "flpEight";
+            this.flpEight.Size = new System.Drawing.Size(150, 150);
+            this.flpEight.TabIndex = 88;
+            // 
+            // flpSeven
+            // 
+            this.flpSeven.BackColor = System.Drawing.Color.DimGray;
+            this.flpSeven.Location = new System.Drawing.Point(3, 307);
+            this.flpSeven.Margin = new System.Windows.Forms.Padding(0);
+            this.flpSeven.Name = "flpSeven";
+            this.flpSeven.Size = new System.Drawing.Size(150, 150);
+            this.flpSeven.TabIndex = 87;
+            // 
+            // flpNine
+            // 
+            this.flpNine.BackColor = System.Drawing.Color.DimGray;
+            this.flpNine.Location = new System.Drawing.Point(307, 307);
+            this.flpNine.Margin = new System.Windows.Forms.Padding(0);
+            this.flpNine.Name = "flpNine";
+            this.flpNine.Size = new System.Drawing.Size(150, 150);
+            this.flpNine.TabIndex = 89;
             // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 470);
-            this.Controls.Add(this.pbxPlayingBoard);
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(460, 460);
+            this.Controls.Add(this.flpEight);
+            this.Controls.Add(this.flpSeven);
+            this.Controls.Add(this.flpNine);
+            this.Controls.Add(this.flpFive);
+            this.Controls.Add(this.flpFour);
+            this.Controls.Add(this.flpSix);
+            this.Controls.Add(this.flpTwo);
+            this.Controls.Add(this.flpOne);
+            this.Controls.Add(this.flpThree);
             this.Name = "Form";
             this.Text = "UltimateTicTacToe";
-            ((System.ComponentModel.ISupportInitialize)(this.pbxPlayingBoard)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pbxPlayingBoard;
+        private System.Windows.Forms.FlowLayoutPanel flpOne;
+        private System.Windows.Forms.FlowLayoutPanel flpTwo;
+        private System.Windows.Forms.FlowLayoutPanel flpThree;
+        private System.Windows.Forms.FlowLayoutPanel flpFive;
+        private System.Windows.Forms.FlowLayoutPanel flpFour;
+        private System.Windows.Forms.FlowLayoutPanel flpSix;
+        private System.Windows.Forms.FlowLayoutPanel flpEight;
+        private System.Windows.Forms.FlowLayoutPanel flpSeven;
+        private System.Windows.Forms.FlowLayoutPanel flpNine;
     }
 }
 
--- UltimateTicTacToe/Form.cs
+++ UltimateTicTacToe/Form.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Net;
 using System.Windows.Forms;
 
 namespace UltimateTicTacToe
 {
     /*
-     - Gör 9st 3x3 pbx ist för en stor
      - Gör en klasser 
         - en för varje liten ruta (81st)
            - pbx, bool träff (är rutan använd), bool isCircle (cross eller cirkle)
@@ -18,193 +15,46 @@ namespace UltimateTicTacToe
 
     public partial class Form : System.Windows.Forms.Form
     {
-        List<int[]> usedSquares = new List<int[]>();
-        List<int> playableGrids = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int spaceBetweenTargets = 51;
-        int targetSize = 48;    // storlke på markörerna
-        int turn = 0;           // 0 == Circle, 1 == Cross
-        int gridPosX;           // vilken box i x-led
-        int gridPosY;           // vilken box i y-led
-        int targetStartPosX;    // vilket x-värde markören ska placeras på
-        int targetStartPosY;    // vilket y-värde markören ska placeras på
+
+        bool circleTurn = true;
 
         public Form()
         {
             InitializeComponent();
 
-            pbxPlayingBoard.SendToBack();
+            EventHandlerPictureboxes(); 
         }
 
-        private void pbxPlayingBoard_MouseClick(object sender, MouseEventArgs e)
+        void EventHandlerPictureboxes()
         {
-            CalculateSquare(e.X, e.Y);
-            int[] placedSquare = { gridPosX, gridPosY };     // placera den använda rutan i en array
-            usedSquares.Add(placedSquare);                   // lägg en array med den använda rutan i en lista 
-
-            PlaceTarget(targetStartPosX, targetStartPosY);
-            
-            Next3x3(gridPosX, gridPosY);
+            foreach (var picturebox in this.Controls.OfType<PictureBox>())
+            {
+                picturebox.BackColor = Color.White;
+                picturebox.Click += PictureBoxClick;
+            }
         }
 
-
-        void CalculateSquare(int mouseX, int mouseY)
-        {
-            double findBox = 472 / 9;
-            gridPosX = (int)(mouseX / findBox);
-            gridPosY = (int)(mouseY / findBox);
-
-            targetStartPosX = FindStartPosX(gridPosX);
-            targetStartPosY = FindStartPosY(gridPosY);
-        }
-
-        void makeCircle(int xPos, int yPos)
+        void PictureBoxClick(object sender, EventArgs e)
         {
             Bitmap circle = new Bitmap(@"C:\Repos\baby-joda\UltimateTicTacToe\Images\Circle.png");
-
-            var Circle = new PictureBox
-            {
-                SizeMode = PictureBoxSizeMode.StretchImage,
-                BackColor = Color.White,
-                Size = new Size(targetSize, targetSize),
-                Location = new Point(xPos, yPos),
-                Image = circle,
-            };
-
-            this.Controls.Add(Circle);
-            Circle.BringToFront();
-        }
-
-        void makeCross(int xPos, int yPos)
-        {
             Bitmap cross = new Bitmap(@"C:\Repos\baby-joda\UltimateTicTacToe\Images\Cross.png");
 
-            var Cross = new PictureBox
-            {
-                SizeMode = PictureBoxSizeMode.StretchImage,
-                BackColor = Color.White,
-                Size = new Size(targetSize, targetSize),
-                Location = new Point(xPos, yPos),
-                Image = cross,
-            };
+            var pictureBoxName = ((PictureBox)sender).Name;
+            PictureBox selected = (PictureBox)this.Controls[pictureBoxName];
+            selected.SizeMode = PictureBoxSizeMode.StretchImage;
 
-            this.Controls.Add(Cross);
-            Cross.BringToFront();
-        }
-
-        int FindStartPosX(int posX)
-        {
-            int targetPosX;
-
-            if (posX >= 0 && posX <= 2)
+            if (circleTurn)
             {
-                targetPosX = (posX * 51) + 4;
-            }
-            else if (posX >= 3 && posX <= 5)
-            {
-                targetPosX = (posX * 51) + 7;
-            }
-            else if (posX == 8)
-            {
-                targetPosX = (posX * 51) + 11;
+                selected.Image = circle;
+                circleTurn = false;
             }
             else
             {
-                targetPosX = (posX * 51) + 10;
+                selected.Image = cross;
+                circleTurn = true;
             }
 
-            return targetPosX;
+            selected.Enabled = false;
         }
-
-        int FindStartPosY(int posY)
-        {
-            int targetPosY;
-
-            if (posY >= 0 && posY <= 2)
-            {
-                targetPosY = (posY * 51) + 4;
-            }
-            else if (posY >= 3 && posY <= 5)
-            {
-                targetPosY = (posY * 51) + 7;
-            }
-            else if (posY == 8)
-            {
-                targetPosY = (posY * 51) + 11;
-            }
-            else
-            {
-                targetPosY = (posY * 51) + 10;
-            }
-
-            return targetPosY;
-        }
-
-        void Next3x3(int gridPosX, int gridPosY)
-        {
-            if ((gridPosX == 0 || gridPosX == 3 || gridPosX == 6) && (gridPosY == 0 || gridPosY == 3 || gridPosY == 6))
-            {
-                playableGrids = new List<int> { 1 };
-            }
-            else if ((gridPosX == 1 || gridPosX == 4 || gridPosX == 7) && (gridPosY == 0 || gridPosY == 3 || gridPosY == 6))
-            {
-                playableGrids = new List<int> { 2 };
-            }
-            else if ((gridPosX == 2 || gridPosX == 5 || gridPosX == 8) && (gridPosY == 0 || gridPosY == 3 || gridPosY == 6))
-            {
-                playableGrids = new List<int> { 3 };
-            }
-            else if ((gridPosX == 0 || gridPosX == 3 || gridPosX == 6) && (gridPosY == 1 || gridPosY == 4 || gridPosY == 7))
-            {
-                playableGrids = new List<int> { 4 };
-            }
-            else if ((gridPosX == 1 || gridPosX == 4 || gridPosX == 7) && (gridPosY == 1 || gridPosY == 4 || gridPosY == 7))
-            {
-                playableGrids = new List<int> { 5 };
-            }
-            else if ((gridPosX == 2 || gridPosX == 5 || gridPosX == 8) && (gridPosY == 1 || gridPosY == 4 || gridPosY == 7))
-            {
-                playableGrids = new List<int> { 6 };
-            }
-            else if ((gridPosX == 0 || gridPosX == 3 || gridPosX == 6) && (gridPosY == 2 || gridPosY == 5 || gridPosY == 8))
-            {
-                playableGrids = new List<int> { 7 };
-            }
-            else if ((gridPosX == 1 || gridPosX == 4 || gridPosX == 7) && (gridPosY == 2 || gridPosY == 5 || gridPosY == 8))
-            {
-                playableGrids = new List<int> { 8 };
-            }
-            else if ((gridPosX == 2 || gridPosX == 5 || gridPosX == 8) && (gridPosY == 2 || gridPosY == 5 || gridPosY == 8))
-            {
-                playableGrids = new List<int> { 9 };
-            }
-        }
-
-
-        void PlaceTarget(int xPos, int yPos)
-        {
-            if (turn == 0)
-            {
-                makeCircle(xPos, yPos);
-                turn = 1;
-            }
-            else
-            {
-                makeCross(xPos, yPos);
-                turn = 0;
-            }
-        }
-
-        /*
-
-        
-        bool Is3x3Full()
-        {
-            if (playableGrids[0] == { 0, 0 })
-            {
-
-            }
-            
-        }
-        */
     }
 }
